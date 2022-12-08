6fd553efb4b9d3c088d71e094f6ba296bf766f92
pedromlls <88161370+PedroMlls@users.noreply.github.com>
2022-12-07 22:24:14 -0300

    Update estoque pedro.sql

--- banco/estoque pedro.sql
+++ banco/estoque pedro.sql
@@ -1,154 +1,48 @@
 /**
-	E-Commerce
-    @author Professor José de Assis
+* Controle de estoque
+* @version 1.2
 */
 
-create database dbloja;
-use dbloja;
+create database dbestoque;
+use dbestoque;
 
-create table clientes (
-	idcli int primary key auto_increment,
-    nome varchar(50) not null,
-    email varchar(50) unique not null,
-    senha varchar(250) not null
+create table usuarios(
+	id int primary key auto_increment,
+    usuario varchar(50) not null,
+    login varchar(20) not null unique,
+    senha varchar(250) not null,
+    perfil varchar(50) not null
 );
 
-insert into clientes (nome,email,senha)
-values ('José de Assis','jose@email.com',md5('123456'));
+describe usuarios;
 
-describe clientes;
-select * from clientes;
-
--- timestamp default current_timestamp (data e hora automático)
--- date (tipo de dados relacionados a data) YYYYMMDD
-create table produtos (
-	codigo int primary key auto_increment,
-    barcode varchar(50) unique,
-    produto varchar(100) not null,
-    fabricante varchar(100) not null,
-    datacad timestamp default current_timestamp,
-    dataval date not null,
-    estoque int not null,
-    estoquemin int not null,
-    medida varchar(50) not null,
-    valor decimal(10,2),
-    loc varchar(100)
+create table fornecedores (
+	idFor int primary key auto_increment,
+	razaoSocial varchar(50) not null,
+	fantasia varchar(50) not null,
+	cnpj varchar(20) unique,
+	ie varchar(20) unique,
+	cep varchar(10) not null,
+	endereco varchar(50) not null,
+	numero varchar(6) not null,
+	complemento varchar(20),
+	bairro varchar(50) not null,
+	cidade varchar(50) not null,
+	uf char(2) not null,
+	nomeContato varchar(30) not null,
+	fone varchar(15) not null,
+	whatsapp varchar(15),
+	email varchar(50) not null,
+	site varchar(50),
+	obs varchar(250)
 );
 
-describe produtos;
+describe fornecedores;
 
-insert into produtos(produto,fabricante,dataval,estoque,estoquemin,
-medida,valor,loc)
-values
-('Caneta BIC azul','BIC',20221005,100,10,'CX',28.75,'setor A p2');
+insert into fornecedores (razaoSocial, fantasia, cnpj, ie, cep, endereco, numero, complemento, bairro, cidade, uf, nomeContato, fone, whatsapp, email, site, obs) values ('Kalunga', 'Kalunga', '43.283.811/0059-76', '206292929110', '70711-000', 'Rua dos Kalungas', '1000', '', 'Distrito Asa Norte', 'Brasilia', 'DF', 'Luiz Carlos', '1234-5678', '1234-0000', 'luiz.carlos@kalunga.com.br', 'www.kalunga.com.br', 'Pode pá');
+insert into fornecedores (razaoSocial, fantasia, cnpj, ie, cep, endereco, numero, complemento, bairro, cidade, uf, nomeContato, fone, whatsapp, email, site, obs) values ('Kalango', 'Kalango', '44.283.811/0059-76', '207292929110', '70712-000', 'Rua dos Calangos', '2000', '', 'Calango do Norte', 'Amapá', 'AP', 'João Pedro', '1111-0000', '2222-0000', 'joao.pedro@kalango.com.br', 'www.kalango.com.br', 'Calanguinho');
+insert into fornecedores (razaoSocial, fantasia, cnpj, ie, cep, endereco, numero, complemento, bairro, cidade, uf, nomeContato, fone, whatsapp, email, site, obs) values ('Kabum', 'Kabum', '45.283.811/0059-76', '207292929111', '70713-000', 'Rua do Kabum', '3000', '', 'Cambuquira', 'Londrina', 'PR', 'Maria do Carmo', '3333-0000', '4444-0000', 'maria.carmo@kabum.com.br', 'www.kabum.com.br', 'Explosao');
+insert into fornecedores (razaoSocial, fantasia, cnpj, ie, cep, endereco, numero, complemento, bairro, cidade, uf, nomeContato, fone, whatsapp, email, site, obs) values ('Furacão', 'Furacao', '46.283.811/0059-76', '206292929112', '80711-000', 'Rua das Explosões', '4000', '', 'Explosivo', 'Blumenau', 'SC', 'Carlos Magno', '5555-0000', '6666-0000', 'carlos.magno@furacao.com.br', 'www.furacao.com.br', 'Ventania');
+insert into fornecedores (razaoSocial, fantasia, cnpj, ie, cep, endereco, numero, complemento, bairro, cidade, uf, nomeContato, fone, whatsapp, email, site, obs) values ('Fusca', 'Fusca', '47.283.811/0059-76', '206292929113', '90711-000', 'Rua dos carros', '5000', '', 'Fuscão Preto', 'Rocinha', 'RJ', 'Zé Pequeno', '7777-0000', '8888-0000', 'ze.pequeno@fusca.com.br', 'www.fusca.com.br', 'Vrum');
 
-insert into produtos(produto,barcode,fabricante,dataval,estoque,estoquemin,
-medida,valor,loc)
-values
-('Régua Jojo 30cm','2222222222','JJBA',20221006,50,5,'UN',3.40,'setor A p3');
-
-insert into produtos(produto,barcode,fabricante,dataval,estoque,estoquemin,
-medida,valor,loc)
-values
-('Cola bastão','3333333333','Tenaz',20210506,50,5,'CX',1,'setor B p1');
-
-insert into produtos(produto,barcode,fabricante,dataval,estoque,estoquemin,
-medida,valor,loc)
-values
-('Massa de modelar ABC','4444444444','Toki',20200910,10,5,'UN',4.40,'setor C p1');
-
-insert into produtos(produto,barcode,fabricante,dataval,estoque,estoquemin,
-medida,valor,loc)
-values
-('Lápis preto CX 20','5555555555','Presto',20221006,10,20,'CX',5,'setor A p3');
-
-insert into produtos(produto,barcode,fabricante,dataval,estoque,estoquemin,
-medida,valor,loc)
-values
-('Borracha branca','6666666666','pirelli',20231006,15,50,'UN',0.50,'setor A p4');
-
-select * from produtos;
-
--- Inventário do estoque(total)
-select sum(estoque * valor) as total from produtos;
-
--- Relatório de reposição do estoque 1
-select * from produtos where estoque < estoquemin;
-
--- Relatório de reposição do estoque 2
--- date_format() -> formatar a exibição da data
--- %d (dia) %m (mês) %y (ano 2 dígitos) %Y (ano 4 dígitos)
-select codigo as código,produto,
-date_format(dataval,'%d/%m/%Y') as data_validade,
-estoque, estoquemin as estoque_mínimo
-from produtos where estoque < estoquemin;
-
--- Relatório de validade dos produtos 1
-select codigo as código,produto,
-date_format(dataval,'%d/%m/%Y') as data_validade
-from produtos;
-
--- Relatório de validade dos produtos 2
--- datediff() retorna a diferença em dias de duas datas
--- curdate() data atual
-select codigo as código, produto,
-date_format(dataval,'%d/%m/%Y') as data_validade,
-datediff(dataval,curdate()) as dias_restantes
-from produtos;
-
--- decimal (tipo de dado numérico não inteiro 10,2 dez digitos com 2 casas decimais de precisão)
-create table pedidos(
-	pedido int primary key auto_increment,
-    dataped timestamp default current_timestamp,
-    total decimal(10,2),
-    idcli int not null,
-    foreign key(idcli) references clientes(idcli)
-);
-
-insert into pedidos(idcli) values(1);
-select * from pedidos;
-
--- abertura do pedido
-select
-pedidos.pedido,
-date_format(pedidos.dataped,'%d/%m/%Y - %H:%i') as data_pedido,
-clientes.nome as cliente, clientes.email as e_mail
-from pedidos inner join clientes
-on pedidos.idcli = clientes.idcli;
-
-create table carrinho (
-	pedido int not null,
-    codigo int not null,
-    quantidade int not null,
-    foreign key(pedido) references pedidos(pedido),
-    foreign key(codigo) references produtos(codigo)
-);
-
-select * from carrinho;
-
-insert into carrinho (pedido,codigo,quantidade)
-values (1,1,10);
-insert into carrinho (pedido,codigo,quantidade)
-values (1,2,5);
-
--- exibir o carrinho
-select
-pedidos.pedido,
-carrinho.codigo as código,
-produtos.produto,
-carrinho.quantidade,
-produtos.valor,
-produtos.valor * carrinho.quantidade as sub_total
-from (carrinho inner join pedidos on carrinho.pedido = pedidos.pedido)
-inner join produtos on carrinho.codigo = produtos.codigo;
-
--- total do carrinho
-select sum(produtos.valor * carrinho.quantidade) as total
-from carrinho inner join produtos on carrinho.codigo = produtos.codigo;
-
--- atualizar o estoque após o fechamento do pedido
-update carrinho
-inner join produtos
-on carrinho.codigo = produtos.codigo
-set produtos.estoque = produtos.estoque - carrinho.quantidade
-where carrinho.quantidade > 0;
+select * from fornecedores;
